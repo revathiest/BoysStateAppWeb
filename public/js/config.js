e1262d85b0172af9085089f1a1670a44c9b65ceb
revathiest <revathiest@gmail.com>
2025-08-02 18:51:34 -0500

    removed ignore flags from modules

--- public/js/config.js
+++ public/js/config.js
@@ -1,3 +1,2 @@
-/* istanbul ignore file */
-window.API_URL = "https://boysstateappservices.up.railway.app";
-// window.API_URL = "http://localhost:3000";
+// window.API_URL = "https://boysstateappservices.up.railway.app";
+window.API_URL = "http://localhost:3000";
