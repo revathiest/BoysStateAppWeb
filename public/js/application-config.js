910cb7eaf6f5b824d444cda63c223e21e1ee3613
revathiest <revathiest@gmail.com>
2026-01-27 19:05:00 -0600

    More fixes

--- public/js/application-config.js
+++ public/js/application-config.js
@@ -274,7 +274,7 @@ let currentType = 'delegate';
       if (typeof programId !== 'undefined' && programId && currentYear && currentType) {
         const token = encodeToken(currentYear, currentType);
         document.getElementById('publicApplicationUrl').value =
-          location.origin + '/public/apply.html?programId=' + encodeURIComponent(programId) + '&token=' + encodeURIComponent(token);
+          location.origin + '/apply.html?programId=' + encodeURIComponent(programId) + '&token=' + encodeURIComponent(token);
       } else {
         document.getElementById('publicApplicationUrl').value =
           'Program ID not available.';
